f83dfc9aca5f3bf1cacc724c90a242226720cf8f
Rishavsingh kshatriya <rishavsinghkshatriya@Rishavsinghs-MacBook-Pro.local>
2024-09-18 00:43:12 -0400

    rename package

--- src/main/java/com/ecommerce/ecommerce_api/EcommerceApiApplication.java
+++ src/main/java/com/ecommerce/ecommerce_api/EcommerceApiApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce;
+package com.ecommerce.ecommerce_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ecommerce/ecommerce_api/EcommerceApiApplicationTests.java
+++ src/test/java/com/ecommerce/ecommerce_api/EcommerceApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ecommerce;
+package com.ecommerce.ecommerce_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
